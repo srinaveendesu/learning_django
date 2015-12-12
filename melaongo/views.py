587c53b6f30e506fd072f231d7e65e059374dac2
srinaveen desu <srinaveen.desu@gmail.com>
2015-12-13 02:02:18 +0530

    blog URL corrected

--- melaongo/views.py
+++ melaongo/views.py
@@ -14,13 +14,13 @@ from django.contrib.auth.decorators import login_required
 #######################################################################
 
 
-def blog(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/blog.html', {'posts': posts})
-
-def blog_post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post.html', {'post': post})
+##def blog(request):
+##    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+##    return render(request, 'blog/blog.html', {'posts': posts})
+##
+##def blog_post_detail(request, pk):
+##    post = get_object_or_404(Post, pk=pk)
+##    return render(request, 'blog/post.html', {'post': post})
 
 
 #######################################################################
