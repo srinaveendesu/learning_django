5185858b06e6b55a6001ee86fd5119e4b7d37242
srinaveen desu <srinaveen.desu@gmail.com>
2015-12-16 01:24:20 +0530

    modified edit and add post page for better User exp

--- melaongo/views.py
+++ melaongo/views.py
@@ -54,7 +54,7 @@ def post_new(request):
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})
+    return render(request, 'blog/post_add.html', {'form': form})
 
 @login_required
 def post_edit(request, pk):
