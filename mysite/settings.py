8010564cd185457e4ecffe9eddd2bba91623cdde
srinaveen desu <srinaveen.desu@gmail.com>
2016-01-22 01:44:44 +0530

    added comment and like button to blog detail page

--- mysite/settings.py
+++ mysite/settings.py
@@ -114,7 +114,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 DEFAULT_FROM_EMAIL = 'testing@example.com'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = 'srinaveen.desu'
-EMAIL_HOST_PASSWORD = 'Kinnu@1991'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
