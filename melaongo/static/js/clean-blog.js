4c574d32fe0e590f2f0f973ff6bfdcee3ee68f1a
srinaveen desu <srinaveen.desu@gmail.com>
2016-03-03 01:06:15 +0530

    added scroll to top feature

--- melaongo/static/js/clean-blog.js
+++ melaongo/static/js/clean-blog.js
@@ -1030,3 +1030,29 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+$(document).ready(function(){
+
+$(function(){
+ 
+    $(document).on( 'scroll', function(){
+ 
+    	if ($(window).scrollTop() > 100) {
+			$('.scroll-top-wrapper').addClass('show');
+		} else {
+			$('.scroll-top-wrapper').removeClass('show');
+		}
+	});
+ 
+	$('.scroll-top-wrapper').on('click', scrollToTop);
+});
+ 
+function scrollToTop() {
+	verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+	element = $('body');
+	offset = element.offset();
+	offsetTop = offset.top;
+	$('html, body').animate({scrollTop: offsetTop}, 500, 'linear');
+}
+
+});
